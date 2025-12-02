0202b9ccf1574f35d4789950d4adaa181e2accb9
yoorrll <lyl1567@naver.com>
2025-12-02 13:36:27 +0900

    feat: Page 컴포넌트 이름 변경

--- src/app/(protected)/gallery/page.jsx
+++ src/app/(protected)/gallery/page.jsx
@@ -2,7 +2,7 @@ import PhotoCardList from '@/components/ui/card/PhotoCardList';
 import GalleryFilter from './_components/GalleryFilter';
 import CardGradeStatus from '@/components/ui/label/CardGradeStatus';
 
-export default function MyGallery() {
+export default function MyGalleryPage() {
   return (
     <div className="sm:mb-[80px] md:mb-[110px] lg:mb-[140px]">
       <CardGradeStatus />
