fd443fc77c7156042cc697b1842644b913e8f06b
yoorrll <lyl1567@naver.com>
2025-12-12 10:22:05 +0900

    feat: 페이지 이동시 필터링 결과 리셋

--- src/components/ui/search/SearchBar.jsx
+++ src/components/ui/search/SearchBar.jsx
@@ -3,9 +3,15 @@
 import Image from 'next/image';
 import ic_search from '@/assets/icons/Ic_search.svg';
 import { useFilter } from '@/providers/FilterProvider';
+import { useEffect } from 'react';
 
-export default function SearchBar() {
+export default function SearchBar({ isGallery = false, isSellingPage = false }) {
   const { searchKeyword, setSearchKeyword } = useFilter();
+
+  // 페이지 바뀌면 필터 리셋
+  useEffect(() => {
+    setSearchKeyword('');
+  }, [isGallery, isSellingPage]);
 
   const handleChange = (e) => {
     setSearchKeyword(e.target.value);
