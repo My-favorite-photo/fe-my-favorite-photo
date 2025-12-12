bda39ca1138c9eb5d91e99a4b0528d405c6043d9
yoorrll <lyl1567@naver.com>
2025-12-12 17:40:41 +0900

    feat: 마켓 플레이스 헤더 & 버튼 반응형 정상화

--- src/app/(public)/market-place/_components/CardTitleClient.jsx
+++ src/app/(public)/market-place/_components/CardTitleClient.jsx
@@ -1,15 +1,19 @@
-"use client"
-import { CardTitle } from "@/components/common/card-title/CardTitle";
-import { MODAL_TYPES, useModal } from "@/providers/ModalProvider";
+'use client';
+import { CardTitle } from '@/components/common/card-title/CardTitle';
+import { MODAL_TYPES, useModal } from '@/providers/ModalProvider';
 
-export function CardTitleClient() {
-  const { openModal } = useModal()
+export function CardTitleClient({ size }) {
+  const { openModal } = useModal();
   return (
     <>
       <CardTitle
         onClick={() => openModal(MODAL_TYPES.SELL_PHOTO_CARD)}
         titleMessage="마켓 플레이스"
         buttonMessage="나의 포토카드 판매하기"
-      /></>
-  )
+        className="w-full mx-auto"
+        size={size ? 'M' : ''}
+        buttonSize={size ? 'S' : ''}
+      />
+    </>
+  );
 }
