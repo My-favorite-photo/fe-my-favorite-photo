faa1153876e4bf270c3f5d4991071d1c47e6c289
rklpoi5678 <rklpoi5678@naver.com>
2025-12-14 23:47:49 +0900

    Feat: 승인하기 버튼시 거래가 성사되게 추가

--- src/components/ui/card/PhotoCardExchange.jsx
+++ src/components/ui/card/PhotoCardExchange.jsx
@@ -1,7 +1,8 @@
+'use client'
 import Image from 'next/image';
 
-import ExchangeImg from '@/assets/images/Img_exchange.png';
-import BeachImg from '@/assets/images/svg/img_beach.png';
+import { tradeService } from '@/libs/services/tradeService';
+import { MODAL_TYPES, useModal } from '@/providers/ModalProvider';
 
 import { Button } from '../button/Button';
 import GradeLabel from '../label/GradeLabel';
@@ -15,20 +16,66 @@ function ResponsiveText({ mobile, desktop }) {
   );
 }
 
-export function CardExchange({ grade = 'COMMON', onApprove = () => { }, onReject = () => { } }) {
-  const cardImage = grade === 'SUPER_RARE' ? BeachImg : ExchangeImg;
+export function CardExchange({ tradeItem }) {
+  const { openModal, closeModal } = useModal()
+  const offeredCardInfo = tradeItem.tradeHistories[0].userCard.photoCard;
+
+
+  const baseHost = process.env.NEXT_PUBLIC_IMAGE_HOST || 'http://127.0.0.1:3005';
+  const fullImageUrl = offeredCardInfo.imageUrl
+    ? offeredCardInfo.imageUrl.startsWith('http')
+      ? offeredCardInfo.imageUrl
+      : `${baseHost}/${offeredCardInfo.imageUrl}`
+    : Img_Card; // 기본 이미지는 폴백
+
+  const handleApprove = (tradeId) => {
+    openModal(MODAL_TYPES.MODAL, {
+      title: "교환 제시 승인",
+      description: `${offeredCardInfo.grade} | ${offeredCardInfo.name} 교환 제시를 승인하시겠습니까?`,
+      confirmText: "승인하기",
+      onConfirm: async () => {
+        try {
+          await tradeService.ownerApprove(tradeId)
+          alert("거래가 되었습니다.")
+          closeModal()
+        } catch (error) {
+          console.error("교환 승인 실패:", error)
+          alert("승인을 실패하였습니다.", error)
+        }
+      }
+    })
+  }
+
+  const handleReject = (tradeId) => {
+    openModal(MODAL_TYPES.MODAL, {
+      title: "교환 제시 거절",
+      description: `${offeredCardInfo.grade} | ${offeredCardInfo.name} 교환 제시를 거절하시겠습니까?`,
+      confirmText: "거절하기",
+      onConfirm: async () => {
+        try {
+          await tradeService.ownerReject(tradeId)
+          alert("교환을 거절하였습니다.")
+          closeModal();
+        } catch (error) {
+          console.error("교환 거절 실패:", error)
+          alert("거절을 실패하였습니다.", error)
+        }
+      },
+    })
+  }
+
 
   return (
     <div className="flex flex-col max-w-42.5 max-h-77 p-2.5 border border-[rgba(255,255,255,0.10)] rounded-[2px] sm:max-w-85.5 sm:max-h-140.25 sm:p-5 md:max-w-110 md:max-h-156.5 md:p-10">
       <div className="relative max-w-37.5 max-h-28 aspect-video sm:max-w-75.5 sm:max-h-[14.1563rem] md:max-w-90 md:max-h-67.5">
-        <Image src={cardImage} alt="교환될 사진" fill />
+        <Image src={fullImageUrl} alt="교환될 사진" fill />
       </div>
       <div className="border-b border-gray-400 mt-2.5 sm:mt-[1.5938rem]">
-        <h1 className="text-white text-sm font-bold sm:text-[1.375rem]">스페인 여행</h1>
+        <h1 className="text-white text-sm font-bold sm:text-[1.375rem]">{tradeItem.tradeHistories[0].userCard.photoCard.name}</h1>
         <div className="flex flex-col mb-2.5 sm:mb-5 md:flex-row md:items-center md:justify-between">
           <div className="flex items-center gap-1.25 sm:gap-2.5 sm:mt-2.5 md:flex">
             {/* 컴포넌트 반응형 수정 */}
-            <GradeLabel grade="COMMON" size="sm" />
+            <GradeLabel grade={tradeItem.tradeHistories[0].userCard.photoCard.grade} />
             <div className="text-gray-400">|</div>
             <p className="text-gray-300 text-[.625rem] sm:text-base">풍경</p>
             <div className="text-gray-400 hidden md:block">|</div>
@@ -36,17 +83,17 @@ export function CardExchange({ grade = 'COMMON', onApprove = () => { }, onReject
           <div className="flex justify-between sm:mt-2.5 md:flex-row md:flex-1 md:justify-between md:items-center md:ml-2.5">
             <div className="flex">
               <p className="text-white text-[.625rem] sm:text-base">
-                4 P<span className="text-gray-300 text-[.625rem] sm:text-base">에 구매</span>
+                {tradeItem.tradeHistories[0].userCard.price} P<span className="text-gray-300 text-[.625rem] sm:text-base">에 구매</span>
               </p>
             </div>
-            <p className="text-white underline text-[0.625rem] sm:text-base">프로여행러</p>
+            <p className="text-white underline text-[0.625rem] sm:text-base">{tradeItem.applicant.nickname}</p>
           </div>
         </div>
       </div>
 
       <div className="mt-2.5 mb-5 sm:mt-5 sm:mb-6.25 md:mb-10">
         <p className="text-white text-[.625rem] tracking-tight line-clamp-2 sm:text-base">
-          스페인 여행 사진도 좋은데 우리집 텍스트 넘치면 여기서 eclipse 처리...
+          {tradeItem.description}
         </p>
       </div>
 
@@ -55,14 +102,14 @@ export function CardExchange({ grade = 'COMMON', onApprove = () => { }, onReject
           intent="secondary"
           thickness="thin"
           className="py-4.25 h-10 hover:bg-gray-500 sm:h-13.75 md:h-15"
-          onClick={onReject}
+          onClick={() => handleReject(tradeItem.id)}
         >
           <ResponsiveText mobile="거절" desktop="거절하기" />
         </Button>
         <Button
           thickness="thin"
           className="py-4.25 h-10 hover:bg-main/90 sm:h-13.75 md:h-15"
-          onClick={onApprove}
+          onClick={() => handleApprove(tradeItem.id)}
         >
           <ResponsiveText mobile="승인" desktop="승인하기" />
         </Button>
