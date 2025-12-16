5b221c1c0d23955a2de55ef03f94d7440c5e7ad0
yoorrll <lyl1567@naver.com>
2025-12-16 13:39:40 +0900

    feat: 모바일 헤더 적용 테스트

--- src/app/(public)/layout.jsx
+++ src/app/(public)/layout.jsx
@@ -1,15 +1,21 @@
-"use client"
-import { usePathname } from "next/navigation";
+'use client';
+import { usePathname } from 'next/navigation';
 
-import Header from "@/components/common/header/Header";
-import { publicMetadata } from "#/config/metadata";
+import Header from '@/components/common/header/Header';
+import { publicMetadata } from '#/config/metadata';
+import MobileHeader from '@/components/common/header/MobileHeader';
 
 export default function Layout({ children }) {
-  const pathname = usePathname()
-  const isCompletePage = pathname.endsWith('/sell-detail')
+  const pathname = usePathname();
+  const isCompletePage = pathname.endsWith('/sell-detail');
   return (
     <>
-      {!isCompletePage && <Header />}
+      {!isCompletePage && (
+        <>
+          <Header />
+          <MobileHeader />
+        </>
+      )}
       {children}
     </>
   );
