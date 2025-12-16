f248496dfdc58e7f1bbe1c1aa5ed5a8babdf861c
rklpoi5678 <rklpoi5678@naver.com>
2025-12-16 22:21:34 +0900

    Feat: 배포를 위한 클라이언트 컴포넌트와 서버컴포넌트 분리 2

--- src/app/(protected)/selling/complete/fail/page.jsx
+++ src/app/(protected)/selling/complete/fail/page.jsx
@@ -1,23 +1,11 @@
-'use client';
+import { Suspense } from "react";
 
-import { useSearchParams } from 'next/navigation';
-
-import ResultPage from '@/components/ui/resultPage/ResultPage';
+import { SellingFailContent } from './_components/SellingFailContent'
 
 export default function SellingFailPage() {
-  const params = useSearchParams();
-  const name = params.get('name');
-
   return (
-    <ResultPage
-      title="판매등록"
-      highlight="실패"
-      name={name}
-      grade=""
-      description="판매 등록에 실패했습니다."
-      highlightColor="text-gray-300"
-      buttonText="마켓플레이스로 돌아가기"
-      buttonHref="/market-place"
-    />
-  );
+    <Suspense fallback={<div>로딩 중...</div>}>
+      <SellingFailContent />
+    </Suspense>
+  )
 }
