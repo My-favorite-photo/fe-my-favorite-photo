74ec67e82a78eec1e175dcca45284ea28e2e305c
yoorrll <143619990+yoorrll@users.noreply.github.com>
2025-12-17 19:34:34 +0900

    Update notificationService.js

--- src/libs/services/notificationService.js
+++ src/libs/services/notificationService.js
@@ -2,6 +2,9 @@ import { tokenFetch } from '../utils/fetchClient';
 
 export const notificationService = {
   getLatest: (userId, limit = 5) => {
+    if (!userId) {
+      return Promise.reject(new Error('userId is required'));
+    }
     const params = new URLSearchParams();
     params.append('userId', userId);
     params.append('limit', limit);
